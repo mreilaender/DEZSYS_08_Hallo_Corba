1f4d2085acaf1ce2ff7ca23eace9437a3757e25c
Michael Borko <michael@borko.at>
2014-12-11 23:40:03 +0100

    Added JacORB Library

---
+++ JacORB/jacorb-3.4/demo/hello/build/generated/demo/hello/GoodDay.java
@@ -0,0 +1,13 @@
+package demo.hello;
+
+/**
+ * Generated from IDL interface "GoodDay".
+ *
+ * @author JacORB IDL compiler V 3.4
+ * @version generated at Dec 11, 2014 9:00:04 PM
+ */
+
+public interface GoodDay
+	extends GoodDayOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity
+{
+}
---
+++ JacORB/jacorb-3.4/demo/hello/build/generated/demo/hello/GoodDayOperations.java
@@ -0,0 +1,17 @@
+package demo.hello;
+
+
+/**
+ * Generated from IDL interface "GoodDay".
+ *
+ * @author JacORB IDL compiler V 3.4
+ * @version generated at Dec 11, 2014 9:00:04 PM
+ */
+
+public interface GoodDayOperations
+{
+	/* constants */
+	/* operations  */
+	java.lang.String hello_simple();
+	java.lang.String hello_wide(java.lang.String msg);
+}
