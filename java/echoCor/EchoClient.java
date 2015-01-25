e5dcfcc9b1412e85d437066c893e6e793bb17da2
mreilaender <mreilaender@student.tgm.ac.at>
2015-01-25 15:56:09 +0100

    Client

---
+++ java/echoCor/EchoClient.java
@@ -0,0 +1,43 @@
+package echoCor;
+
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.omg.CORBA.*;
+import org.omg.CORBA.ORBPackage.InvalidName;
+
+public class EchoClient {
+
+	public static void main(String[] args) {
+		String ns = "NameService";
+		org.omg.CORBA.Object ref;
+		Echo hello;
+		ORB orb = ORB.init(args, null);
+		try {
+			ref = orb.resolve_initial_references(ns);
+			NamingContext namCon = NamingContextExtHelper.narrow(ref);
+			
+			NameComponent com1 = new NameComponent("test", "my_context");
+			NameComponent com2 = new NameComponent("Echo", "Object");
+			
+			NameComponent path[] = {com1, com2};
+			hello = EchoHelper.narrow(namCon.resolve(path));
+			
+			System.out.println("");
+			String returnText = hello.echoString("Hello C++");
+			
+			System.out.println("CPP-Corba returned: " + returnText);
+		} catch (InvalidName e) {
+			System.err.println("Couldn't find naming Service: " + ns + ", because " + e.getMessage());
+		} catch (NotFound e) {
+			System.err.println("Couldn't find Object-Ref(" + e.getMessage() + ")");
+			e.printStackTrace();
+		} catch (CannotProceed e) {
+			// TODO
+			System.err.println("TODO Catch-Clause in HelloClient " + e.getMessage());
+		} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
+			// TODO
+			System.err.println("Invalid ContextPackage: " + e.getMessage());
+		}
+	}
+}
